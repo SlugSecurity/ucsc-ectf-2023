b01d289d78fb804b39caa0140dda1104d3692d7a
Ferns <jeffreyzhang123@gmail.com>
2023-01-19 02:50:08 -0800

    Changed tag name since it's going to be the same for all the projects

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 mkdir -p cache/cargo
-docker build --tag rust-template .
-docker run --rm --privileged -v $(pwd)/:/mnt -it rust-template
+docker build --tag ectf .
+docker run --rm --privileged -v $(pwd)/:/mnt -it ectf
--- run_podman.sh
+++ run_podman.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 mkdir -p cache/cargo
-podman build --tag rust-template .
-podman run --rm --privileged -v $(pwd)/:/mnt -it rust-template
+podman build --tag ectf .
+podman run --rm --privileged -v $(pwd)/:/mnt -it ectf
