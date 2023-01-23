da966b7eda28bb27e6c433f20ff1bf401b3dd7b8
Steven Mak <stmak@ucsc.edu>
2023-01-22 23:14:20 -0800

    Document ListenerError

--- util/src/listener/error.rs
+++ util/src/listener/error.rs
@@ -1,13 +1,19 @@
 use core::fmt::{self, Display, Formatter};
 
+pub type Result<T> = core::result::Result<T, ListenerError>;
+
+/// An error that occurs in the operations of [`Listener`](crate::listener::Listener)s.
 #[non_exhaustive]
 #[derive(Debug, Copy, Clone)]
 pub enum ListenerError {
-    TickError(&'static str), // Write doc header above later, not in variants. This is for errors caused by the tick implementation for the listener.
+    /// An error that occurs while the listener polls for an event.
+    TickError(&'static str),
 }
 
 impl Display for ListenerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        todo!()
+        match self {
+            ListenerError::TickError(err_msg) => err_msg.fmt(f),
+        }
     }
 }
