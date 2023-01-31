e719431a561d8a9a4171bd064d884f04f4c9a748
Brian Mak <eggrollbot@gmail.com>
2023-01-30 20:31:24 -0800

    Change char to unsigned char (u8), add lib prefix to library

--- rand_uninit_memory/rand_uninit_memory.c
+++ rand_uninit_memory/rand_uninit_memory.c
@@ -5,10 +5,10 @@
 
 #define LARGE_STACK_OFFSET 20480 // 20 KiB
 
-char random_bytes[RANDOM_BYTES_SIZE];
+unsigned char random_bytes[RANDOM_BYTES_SIZE];
 
 void __attribute__((noinline)) init_random_bytes(void) {
-    volatile char uninit_bytes[RANDOM_BYTES_SIZE + LARGE_STACK_OFFSET];
+    volatile unsigned char uninit_bytes[RANDOM_BYTES_SIZE + LARGE_STACK_OFFSET];
 
     for (int i = 0; i < RANDOM_BYTES_SIZE; i++) {
         random_bytes[i] = uninit_bytes[i];
--- rand_uninit_memory/rand_uninit_memory.h
+++ rand_uninit_memory/rand_uninit_memory.h
@@ -2,7 +2,7 @@
 
 #define RANDOM_BYTES_SIZE 1024
 
-extern char random_bytes[RANDOM_BYTES_SIZE];
+extern unsigned char random_bytes[RANDOM_BYTES_SIZE];
 
 // Requires at least 22K of stack memory to function. The caller must guarantee this function is never inlined across FFI boundaries.
 void init_random_bytes(void);
