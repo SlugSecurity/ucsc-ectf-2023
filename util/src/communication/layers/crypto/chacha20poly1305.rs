39424958f8218fa1073c4813baf6306987c8fc08
Steven Mak <stmak@ucsc.edu>
2023-02-07 02:31:31 -0800

    Add chacha20poly1305 channel skeleton

--- util/src/communication/layers/crypto/chacha20poly1305.rs
+++ util/src/communication/layers/crypto/chacha20poly1305.rs
@@ -4,16 +4,16 @@ pub struct Chacha20Poly1305RxChannel<T: RxChannel> {
     channel: T,
 }
 
-// impl<T: RxChannel> Chacha20Poly1305RxChannel<T> {
-//     pub fn new(channel: T) -> Self {
-//         Self { channel }
-//     }
-// }
+impl<T: RxChannel> Chacha20Poly1305RxChannel<T> {
+    pub fn new(channel: T, tx_key: &Key, rx_key: &Key) -> Self {
+        Self { channel }
+    }
+}
 
-// impl<T: RxChannel> RxChannel for Chacha20Poly1305RxChannel<T> {
-//     type RxError = todo!();
+impl<T: RxChannel> RxChannel for Chacha20Poly1305RxChannel<T> {
+    type RxError = chacha20poly1305::Error;
 
-//     fn read(&mut self, dest: &mut [u8]) -> Result<(), Self::RxError> {
-//         todo!()
-//     }
-// }
+    fn read(&mut self, dest: &mut [u8]) -> Result<(), Self::RxError> {
+        todo!()
+    }
+}
