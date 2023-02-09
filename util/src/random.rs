5091c748bc0422f60df4281c96c4ca73e71b6d80
Brian Mak <eggrollbot@gmail.com>
2023-02-08 16:17:42 -0800

    Combine init rng functions

--- util/src/random.rs
+++ util/src/random.rs
@@ -26,10 +26,20 @@ use self::entropy::{Adc, ClockDrift, EntropyHasher, Secret, UninitMemory};
 static MAIN_CSPRNG: OnceCell<Mutex<RefCell<ChaCha20Rng>>> = OnceCell::new();
 static SECONDARY_CSPRNG: OnceCell<Mutex<RefCell<ChaCha20Rng>>> = OnceCell::new();
 
-/// Initializes the main CSPRNG. This initialization will block while gathering entropy.
+/// Initializes the secondary and main CSPRNG. The initialization of the main CSPRNG will block while
+/// gathering entropy. The secondary CSPRNG does not need to block while gathering entropy. The
+/// secondary CSPRNG will have been initialized by the time the main CSPRNG is to be initialized.
 ///
 /// Will do nothing if called more than once.
 pub fn init_rng() {
+    SECONDARY_CSPRNG.get_or_init(|| {
+        Mutex::new(RefCell::new(ChaCha20Rng::from_seed(
+            // We use the secret twice here to make this entropy hash different from the one used to
+            // seed the next piece of "uninitialized" memory.
+            EntropyHasher::<UninitMemory<Secret<Secret<()>>>>::new().hash(),
+        )))
+    });
+
     MAIN_CSPRNG.get_or_init(|| {
         Mutex::new(RefCell::new(ChaCha20Rng::from_seed(
             EntropyHasher::<UninitMemory<Secret<Adc<ClockDrift<()>>>>>::new().hash(),
@@ -48,19 +58,6 @@ pub fn fill_rand_slice(dest: &mut [u8]) {
             .borrow(c)
             .borrow_mut()
             .fill_bytes(dest);
-    });
-}
-
-/// Initializes the secondary CSPRNG. This initialization does not need to block to gather entropy.
-///
-/// Will do nothing if called more than once.
-pub fn init_secondary_rng() {
-    SECONDARY_CSPRNG.get_or_init(|| {
-        Mutex::new(RefCell::new(ChaCha20Rng::from_seed(
-            // We use the secret twice here to make this entropy hash different from the one used to
-            // seed the next piece of "uninitialized" memory.
-            EntropyHasher::<UninitMemory<Secret<Secret<()>>>>::new().hash(),
-        )))
     });
 }
 
