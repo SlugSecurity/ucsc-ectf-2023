18a894b8b35670a177f102759e5dc0da9661533e
Brian Mak <brmak@ucsc.edu>
2023-02-19 13:35:29 -0800

    Make runtime private

--- docker_env/util_no_std/src/lib.rs
+++ docker_env/util_no_std/src/lib.rs
@@ -6,11 +6,11 @@
 pub mod communication;
 pub mod eeprom;
 pub mod hib;
-pub mod runtime;
 pub mod timer;
 
 pub(crate) mod random;
 
-pub use runtime::Runtime;
-pub use runtime::RuntimePeripherals;
+mod runtime;
+
+pub use runtime::*;
 pub use ucsc_ectf_util_common::msg_parsing;
