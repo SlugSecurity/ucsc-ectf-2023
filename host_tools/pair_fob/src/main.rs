c89f8ce33e75c30e3c21ae63aafedd77110579d7
Brian Mak <eggrollbot@gmail.com>
2023-03-06 15:42:07 -0800

    Add pairing ack workaround

--- host_tools/pair_fob/src/main.rs
+++ host_tools/pair_fob/src/main.rs
@@ -3,12 +3,12 @@ use std::time::Duration;
 use clap::Parser;
 use ucsc_ectf_util_std::{
     communication::{self, CommunicationError, RxChannel, TxChannel, VerifiedFramedTcpSocket},
-    messages::{HostToolAck, PairingPin, Uart0Message},
-    timer::StdTimer,
+    messages::{PairingPin, Uart0Message},
+    timer::{StdTimer, Timer},
 };
 
 const RECV_BUFF_LEN: usize = 128;
-const FAILED_PAIRING: &str = "Failed to pair fob";
+const FAILED_PAIRING: &str = "Failed to pair fob.";
 
 #[derive(Parser)]
 struct Args {
@@ -37,14 +37,15 @@ fn pair(pin: PairingPin, unpaired_port: u16, paired_port: u16) -> communication:
 
     let mut buff = [0; RECV_BUFF_LEN];
     let mut timeout_timer = StdTimer::new(Duration::from_secs(5));
-    let resp_len = unpaired_socket.recv_with_data_timeout(&mut buff, &mut timeout_timer)?;
-    let resp = postcard::from_bytes::<Uart0Message>(&buff[..resp_len])
-        .map_err(|_| CommunicationError::RecvError)?;
+    let _ = unpaired_socket.recv_with_data_timeout(&mut buff, &mut timeout_timer);
 
-    match resp {
-        Uart0Message::PairingPinResponse(HostToolAck(true)) => Ok(()),
-        _ => Err(CommunicationError::RecvError),
+    // The stupid bridge is cutting off our acknowledgement messages, so we just check for any
+    // message.
+    if timeout_timer.poll() {
+        return Err(CommunicationError::RecvError);
     }
+
+    Ok(())
 }
 
 fn main() {
